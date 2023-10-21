bac6cd50fd5757bf7f6eb4da830c0b0ab89c5508
Feisal Issack Adan <126093289+Feisal2023@users.noreply.github.com>
2023-10-21 18:20:30 +0300

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,13 @@ import noteRoutes from "./routes";
 import connectBD from "./config/db";
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+  origin: ["https://note-keeping-app-au7w.vercel.app/"],
+  methods: ["POST", "GET", "PUT", "DELETE"],
+  credentials: true
+}
+));
 app.use(express.json());
 app.use(noteRoutes);
 
